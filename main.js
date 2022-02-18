60c312a5d4b783d0fa6864d445f4ca796e2bc6be
stuart tichener <76698984+stuart4404@users.noreply.github.com>
2022-02-18 09:09:48 +0000

    header update

--- main.js
+++ main.js
@@ -1,7 +1,18 @@
 const navToggle = document.querySelector('.nav-toggle');
 const nav = document.querySelector('.nav');
+let btnOpen = false;
 
 
 navToggle.addEventListener('click', () => {
     nav.classList.toggle('nav--visible');
-})
+});
+
+navToggle.addEventListener('click', () => {
+    if(!btnOpen) {
+        navToggle.classList.add('open');
+        btnOpen = true;
+    } else {
+        navToggle.classList.remove('open')
+        btnOpen = false
+    }
+});
